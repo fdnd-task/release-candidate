25bdfd29685316aa1d5289513e86fb5e2a1d4b03
M4TThts123 <70703948+M4TThys123@users.noreply.github.com>
2023-05-10 22:52:33 +0200

    Made PrimsicData02

--- src/views/PrismicData.vue
+++ src/views/PrismicData.vue
@@ -4,16 +4,18 @@
       <h2>Hier Vind je de prismic Data</h2>
     </section>
     <prismic-data1></prismic-data1>
+    <prismic-data2></prismic-data2>
   </section>
 </template>
 
 <script>
 
 import PrismicData1 from "@/components/prismic/PrismicData01.vue";
+import PrismicData2 from "@/components/prismic/PrismicData02.vue";
 
 export default {
   name: 'PrismicData',
-  components: {PrismicData1},
+  components: {PrismicData2, PrismicData1},
 }
 </script>
 
