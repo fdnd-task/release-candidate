5561a3cd91c4de21e50650a9b478f1b14d605489
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2024-01-11 13:46:07 +0100

    betalingenpagina start

--- src/routes/betalingen/+page.svelte
+++ src/routes/betalingen/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Betalingen</h1>
+
+
+<section class="section-tegoed">
+
+    <h2>Tegoed</h2>
+    <p>
+        Dit tegoed gebruik je met je OBA-pas. Je kunt er zowel mee lenen als printen.
+    </p>
+    <p>
+        Hieronder kan je via IDEAL tegoed op je OBA-pas zetten.
+    </p>
+    <div class="tegoed-flex">
+    <h3>Jouw tegoed: €24.50</h3>
+    <button>Toevoegen aan tegoed</button>
+    </div>
+
+</section>
+
+<section class="section-tegoed">
+
+    <h2>Jouw abonnement</h2>
+    <p>
+        Hieronder vind je alles wat je dit jaar nog kan doen met je abonnement.
+    </p>
+    <ul>
+        <li></li>
+    </ul>
+    <div class="tegoed-flex">
+    <button>Abonnement uitbreiden</button>
+    </div>
+
+</section>
+
+<style>
+    div{
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
