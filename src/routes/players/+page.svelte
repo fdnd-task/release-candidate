651d036a135b2ba4a7be739e0b5dfcb96dbd4d95
Sascha-davidson <saschadavidson1@gmail.com>
2024-01-11 13:43:42 +0100

    removal of old techniek

--- src/routes/players/+page.svelte
+++ src/routes/players/+page.svelte
@@ -1,25 +1,3 @@
 <script>
 	import TwoColumnLayout from '$lib/layouts/TwoColumnLayout.svelte';
 </script>
-
-<TwoColumnLayout title={'Players'}>
-	<section slot="sub-title-left">
-		<h2>sub title Left</h2>
-	</section>
-
-	<section slot="sub-title-right">
-		<h2>sub title Right</h2>
-	</section>
-
-	<section slot="left">
-		<h2>Left</h2>
-	</section>
-
-	<section slot="right">
-		<h2>Right</h2>
-	</section>
-
-	<footer slot="footer">
-		<h2>Footer</h2>
-	</footer>
-</TwoColumnLayout>
