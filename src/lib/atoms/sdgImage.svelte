7882ecd7fa68dbdce84d3ffca26549ae3b4a623a
MokhtarAkle <mokhtar.akle@hva.nl>
2024-01-15 15:18:10 +0100

    setup from old project

---
+++ src/lib/atoms/sdgImage.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let data;
+
+	// console.log(data);
+</script>
+
+<img class="susDevGoal" src={data.wish.image.image.url} alt="Sustainable development goals" />
+
+<style>
+	.susDevGoal {
+		max-width: 100px;
+		max-height: 100px;
+		margin: 1em;
+	}
+</style>
---
+++ src/lib/molecules/sdg.svelte
@@ -0,0 +1,27 @@
+<script>
+	import SdgImage from '../atoms/sdgImage.svelte';
+	export let data;
+	// console.log(data.wish.image.url);
+</script>
+
+<section class="container-susDevGoal">
+	<img
+		class="susDevGoal"
+		src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Sustainable_Development_Goal_01NoPoverty.svg/599px-Sustainable_Development_Goal_01NoPoverty.svg.png"
+		alt="sustainable development goal 1"
+	/>
+	<SdgImage {data} />
+</section>
+
+<style>
+	.susDevGoal {
+		max-width: 100px;
+		max-height: 100px;
+		margin: 1em;
+	}
+	.container-susDevGoal {
+		display: flex;
+		flex-direction: row;
+		margin-left: 2em;
+	}
+</style>
