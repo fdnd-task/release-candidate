8c8f80730df14505060363c8158d343788f4d97a
Sascha-davidson <saschadavidson1@gmail.com>
2024-01-15 22:53:53 +0100

    fixed responsive design

--- src/routes/players/new/+page.svelte
+++ src/routes/players/new/+page.svelte
@@ -21,13 +21,13 @@
 	main {
 		display: grid;
 		grid-template-rows: auto auto 1fr auto;
-		grid-template-columns: repeat(2, minmax(0, 1fr));
+		grid-template-columns: 1fr;
 
 		grid-template-areas:
-			'h1 h1'
-			'h2 h2'
-			'form .'
-			'footer footer';
+			'h1'
+			'h2'
+			'form'
+			'footer';
 		column-gap: 2rem;
 		row-gap: 1rem;
 		padding: 4rem 4%;
@@ -61,8 +61,8 @@
 
 	@media (pointer: fine) {
 		section::-webkit-scrollbar-track {
-			border-radius: 10px;
 			margin: 10px 0;
+			border-radius: 10px;
 		}
 
 		section::-webkit-scrollbar {
@@ -72,6 +72,17 @@
 		section::-webkit-scrollbar-thumb {
 			border-radius: 10px;
 			background-color: #a7a7a7;
+		}
+		@media only screen and (min-width: 850px) {
+			main {
+				grid-template-columns: repeat(2, minmax(0, 1fr));
+				grid-template-areas:
+					'h1 h1'
+					'h2 h2'
+					'form .'
+					'footer footer';
+
+			}
 		}
 	}
 </style>
