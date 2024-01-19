0e82bae169a9f40de57a301eef189a8cf81e6f91
Luuk <luukbrauckmann@gmail.com>
2024-01-19 14:31:31 +0100

    Fix onkeydown prevent default

--- src/lib/atoms/Fullscreen/Fullscreen.svelte
+++ src/lib/atoms/Fullscreen/Fullscreen.svelte
@@ -1,55 +1,59 @@
 <script>
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-    export let keyboardShortcutEnabled = true;
+	export let keyboardShortcutEnabled = true;
 
-    let element;
-    let supported;
+	let element;
+	let supported;
 
-    const supportedOptions = [
-        { request: 'requestFullscreen', exit: 'exitFullscreen' }, // Standard
-        { request: 'mozRequestFullScreen', exit: 'mozCancelFullScreen' },// Firefox
-        { request: 'webkitRequestFullscreen', exit: 'webkitExitFullscreen' },// Safari
-        { request: 'msRequestFullscreen', exit: 'msExitFullscreen' } // IE11
-    ]
+	const supportedOptions = [
+		{ request: 'requestFullscreen', exit: 'exitFullscreen' }, // Standard
+		{ request: 'mozRequestFullScreen', exit: 'mozCancelFullScreen' }, // Firefox
+		{ request: 'webkitRequestFullscreen', exit: 'webkitExitFullscreen' }, // Safari
+		{ request: 'msRequestFullscreen', exit: 'msExitFullscreen' } // IE11
+	];
 
-    onMount(() => {
-        // Find the supported method
-        supported = supportedOptions.find((value) => value.request in element);
-    })
+	onMount(() => {
+		// Find the supported method
+		supported = supportedOptions.find((value) => value.request in element);
+	});
 
-    function onKeyDown(event) {
-        if (keyboardShortcutEnabled && event.key === 'f') toggle();
-    }
+	function onKeyDown(event) {
+		if (keyboardShortcutEnabled && event.key === 'f') {
+			toggle(event);
+		}
+	}
 
-    function toggle(_) {
-        if (!supported) return;
+	function toggle(event) {
+		if (!supported) return;
 
-        if (!document.fullscreenElement) {
-            element[supported.request]();
-        } else {
-            document[supported.exit]();
-        }
-    }
+		if (!document.fullscreenElement) {
+			element[supported.request]();
+		} else {
+			document[supported.exit]();
+		}
+
+		event.preventDefault();
+	}
 </script>
 
 <div bind:this={element}>
-    {#if supported}
-        <button on:click={toggle}>Toggle fullscreen</button>
-    {/if}
-    
-    <slot />
+	{#if supported}
+		<button on:click={toggle}>Toggle fullscreen</button>
+	{/if}
+
+	<slot />
 </div>
 
-<svelte:window on:keydown|preventDefault={onKeyDown} />
+<svelte:window on:keydown={onKeyDown} />
 
 <style>
-    div {
-        display: contents;
-    }
+	div {
+		display: contents;
+	}
 
-    button {
-        position: absolute;
-        right: 0;
-    }
+	button {
+		position: absolute;
+		right: 0;
+	}
 </style>
