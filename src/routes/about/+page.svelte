e05f2d9e1072104a6fb6f49390deed66ac90235b
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2024-01-22 21:05:10 +0100

    our ultimate goals intro
    ik heb de intro van deze pagina gemaakt

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,40 +1,12 @@
+<script>
+
+  import AboutHero from "$lib/organisms/AboutHero.svelte";
+  import AboutIntro from "$lib/organisms/AboutIntro.svelte";
+
+</script>
 <svelte:head> <title>INK - About page</title> </svelte:head>
 
- <section class="main" > 
-
-    <div class="title">
-    <h1 class="about">About </h1> <h1 class="us"> Us </h1>
-
-</div>
-
-</section> 
-
-<section> 
+<AboutHero />
+<AboutIntro />
 
 
-</section>
-
-<style> 
-
-.title {
-    display: flex;
-
-}
-
-.main {
-background-image: url("assets/INK-content3.png");
-}
-
-h1 {
-    padding: 0;
-}
-
-.about {
-    color: white;
-}
-
-.us {
-    padding-left: 0.5em;
-    
-}
-</style>
