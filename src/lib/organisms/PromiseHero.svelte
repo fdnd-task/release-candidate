9a48acbf846255444a43505298b3b8f1fbbdde6f
Suuskehh <suus.harsveld@hva.nl>
2024-01-22 16:13:42 +0100

    :construction:
    Mobile promise hero done

--- src/lib/organisms/PromiseHero.svelte
+++ src/lib/organisms/PromiseHero.svelte
@@ -1,16 +1,65 @@
+<script>
+	import { BtnArrow } from '$lib/index.js';
+    import { base } from '$app/paths';
+	// export let data;
+</script>
+
 <section class="heroPromise">
-    <h1>Our promise</h1>
+    <div class="heroPromiseWrapper">
+        <h1>Our <span style=" color: var(--Blue-Secondary);">promise</span></h1>
+        <p>We strive to create a level playing field for all, and we work to ensure that everyone has the opportunity to succeed.</p>
+        <div class="btnArrow">
+            <BtnArrow  href="{base}#introPromise" />
+        </div>
+    </div>
 </section>
 
-
-
-
 <style>
-    .heroPromise {
-        width: 100%;
-        height: 80vh;
-        background-image: url(/assets/PromiseHeroImg800x1200.jpg);
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
+
+.heroPromise {
+    width: 100%;
+    height: 50vh;
+    display: block;
+    text-align: center;
+}
+
+.heroPromise::before {
+  content: "";
+  background: url(/assets/PromiseHeroImg800x1200.jpg) ;
+  background-position: center;
+  background-size: cover;
+  opacity: 0.5;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+  z-index: -1;   
+}
+
+.heroPromiseWrapper {
+    /* width: 80%; */
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -30%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.heroPromiseWrapper > h1 {
+    color: var(--White);
+}
+
+.heroPromiseWrapper > p {
+    width: 80%;
+}
+
+.btnArrow {
+    display: flex;
+    justify-content: center;
+}
+
 </style>
+  
