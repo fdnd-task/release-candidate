6e81b6751c5807573db7d92872dbbe9545844f07
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2024-01-22 15:58:53 +0100

    CardCarrousel is toegevoegd

--- src/lib/index.js
+++ src/lib/index.js
@@ -10,13 +10,7 @@ export { default as NavLink } from './atoms/NavLink.svelte'
 export { default as HeroTitle } from './atoms/HeroTitle.svelte'
 export { default as FooterButton } from './atoms/FooterButton.svelte'
 export { default as Sponsorlabels } from './atoms/Sponsorlabels.svelte'
-export { default as NavLogo } from "./atoms/NavLogo.svelte";
-export { default as BtnPrimary } from "./atoms/BtnPrimary.svelte";
-export { default as BtnSecondary } from "./atoms/BtnSecondary.svelte";
-export { default as NavLink } from "./atoms/NavLink.svelte";
-export { default as HeadTitle } from "./atoms/HeadTitle.svelte";
-export { default as FooterButton } from "./atoms/FooterButton.svelte";
-export { default as Sponsorlabels } from "./atoms/Sponsorlabels.svelte";
+// export { default as HeadTitle } from "./atoms/HeadTitle.svelte";
 export { default as CarrouselButton } from "./atoms/CarrouselButton.svelte";
 export { default as ImageCarrousel } from "./atoms/ImageCarrousel.svelte";
 
@@ -34,27 +28,19 @@ export { default as CardCarrousel } from "./molecules/CardCarrousel.svelte";
 
 export { default as Header } from './organisms/Header.svelte'
 export { default as Sponsorpage } from './organisms/Sponsorpage.svelte'
-
 export { default as MainHero } from './organisms/MainHero.svelte'
 export { default as MainAbout } from './organisms/MainAbout.svelte'
 export { default as PlayerCarrousel } from './organisms/PlayerCarrousel.svelte'
 export { default as MainStats } from './organisms/MainStats.svelte'
 export { default as MainSponsor } from './organisms/MainSponsor.svelte'
 export { default as MainStory } from './organisms/MainStory.svelte'
-
 export { default as PromiseHero } from './organisms/PromiseHero.svelte'
 export { default as PromiseIntro } from './organisms/PromiseIntro.svelte'
-
 export { default as Footer } from './organisms/Footer.svelte'
 
-export { default as Header } from "./organisms/Header.svelte";
-export { default as Sponsorpage } from "./organisms/Sponsorpage.svelte";
 
-export { default as MainHead } from "./organisms/MainHead.svelte";
-export { default as MainAbout } from "./organisms/MainAbout.svelte";
-export { default as PlayerCarrousel } from "./organisms/PlayerCarrousel.svelte";
-export { default as MainStats } from "./organisms/MainStats.svelte";
-export { default as MainSponsor } from "./organisms/MainSponsor.svelte";
-export { default as MainStory } from "./organisms/MainStory.svelte";
+
+// export { default as MainHead } from "./organisms/MainHead.svelte";
+
 export { default as Carrousel } from "./organisms/Carrousel.svelte";
-export { default as Footer } from "./organisms/Footer.svelte";
+
