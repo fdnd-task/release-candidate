64cbea1de7d6bf75384805334dd6e2cb7aaad875
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2024-01-23 13:28:19 +0100

    reset my code because i got a lot of errors

--- src/routes/[zoekresultaten]/+page.svelte
+++ src/routes/[zoekresultaten]/+page.svelte
@@ -5,6 +5,8 @@
     import { Card } from '$lib/index.js';
 </script>
 
+<h1>Uw resultaten voor:</h1>
+
 <section>
     {#each results as result}
     <a href={result.detailLink}>
@@ -15,5 +17,23 @@
         <!-- <h1>{result.frabl.key1}</h1> -->
     </a>
     {/each}
-    {data}
 </section>
+
+
+<style>
+
+    h1{
+        padding: 2rem;
+    }
+
+    section{
+        display: grid;
+        grid-template-columns:repeat(auto-fit, minmax(15em, 1fr));
+        gap: 2rem;
+        padding: 2rem;
+    }
+
+    a{  
+        display: block;
+    }
+</style>
